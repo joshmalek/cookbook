0240c1b2b7cbd5203c7062060caeb4f10ae0d9b6
17jmalek98 <17jmalek98@gmail.com>
2020-05-12 16:58:04 -0700

    added api calls and json output

--- recipe-book/src/App.js
+++ recipe-book/src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import Recipe from './Recipe';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+
+
+  const APP_ID = "d01ab069";
+  const APP_KEY = "483ed0cf505f4faf21e6aed59a28189c";
+
+  const [recipes, setRecipes] = useState([]); //contains all recipes from GET call
+
+  useEffect( () => {
+    getRecipes();
+
+
+  }, []);  //useeffect will only update if item in brackets changes
+
+  const getRecipes = async () => {
+    const response = await fetch( `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipes(data.hits); //sets all returned recipes to state
+    console.log(data.hits);
+  }
+
+  return(
+    <div className = "App">
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">Search</button>
+      </form>
+
+      {recipes.map(recipe =>(
+        <Recipe 
+          title = {recipe.recipe.label} 
+          calories = {recipe.recipe.calories} 
+          image = {recipe.recipe.image}
+        />
+      ))}
+
     </div>
-  );
+  )
+
+
 }
 
 export default App;
---
+++ recipe-book/src/Recipe.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Recipe = ({title,calories,image}) => {
+    return(
+        <div>
+            <h1>{title}</h1>
+            <p>Calories: {calories}</p>
+            <img src={image} alt=""/>
+        </div>
+    )
+}
+
+
+export default Recipe;
